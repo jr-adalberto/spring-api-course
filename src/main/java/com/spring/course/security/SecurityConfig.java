7d37fc15193328625d4aecd0a5ba9bc67edaa4b9
Adalberto da Silva Junior <sbetiin_@hotmail.com>
2025-02-10 20:52:18 -0300

    created security config

---
+++ src/main/java/com/spring/course/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.spring.course.security;
+
+import com.spring.course.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class SecurityConfig {
+
+    private final UserService userService;
+    private final CustomPasswordEncoder passwordEncoder;
+    private final AuthorizationFilter authorizationFilter;
+
+    public SecurityConfig(UserService userService, CustomPasswordEncoder passwordEncoder, AuthorizationFilter authorizationFilter) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.authorizationFilter = authorizationFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/users/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
