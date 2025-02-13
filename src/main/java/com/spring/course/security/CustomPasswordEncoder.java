b134ddb800d45eefd55e52334b5c59755e44f222
Adalberto da Silva Junior <sbetiin_@hotmail.com>
2025-02-13 20:34:41 -0300

    create password encoder

--- src/main/java/com/spring/course/security/CustomPasswordEncoder.java
+++ src/main/java/com/spring/course/security/CustomPasswordEncoder.java
@@ -9,14 +9,12 @@ public class CustomPasswordEncoder implements PasswordEncoder {
 
     @Override
     public String encode(CharSequence rawPassword) {
-        String hash = HashUtil.getSecureHash(rawPassword.toString());
-        return hash;
+        return HashUtil.getSecureHash(rawPassword.toString());
     }
 
     @Override
     public boolean matches(CharSequence rawPassword, String encodedPassword) {
-        String hash = HashUtil.getSecureHash(rawPassword.toString());
-        return hash.equals(encodedPassword);
+        String hashedPassword = HashUtil.getSecureHash(rawPassword.toString());
+        return hashedPassword.equals(encodedPassword);
     }
-
 }
