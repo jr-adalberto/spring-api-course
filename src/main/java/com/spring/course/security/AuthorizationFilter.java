2a52d69011ea5ab2153b7114b80c5c9073741705
Adalberto da Silva Junior <sbetiin_@hotmail.com>
2025-02-17 20:23:22 -0300

    import fixes

--- src/main/java/com/spring/course/security/AuthorizationFilter.java
+++ src/main/java/com/spring/course/security/AuthorizationFilter.java
@@ -1,37 +1,30 @@
 package com.spring.course.security;
 
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.spring.course.constant.SecurityConstants;
-import com.spring.course.resource.exception.ApiError;
-import com.spring.course.resource.exception.ApiErrorList;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class AuthorizationFilter extends OncePerRequestFilter {
 
     private final JwtManager jwtManager;
     private final ObjectMapper objectMapper;
 
-    public AuthorizationFilter(JwtManager jwtManager, ObjectMapper objectMapper) {
+   public AuthorizationFilter(JwtManager jwtManager, ObjectMapper objectMapper) {
         this.jwtManager = jwtManager;
         this.objectMapper = objectMapper;
     }
@@ -42,13 +35,20 @@ public class AuthorizationFilter extends OncePerRequestFilter {
         String token = request.getHeader("Authorization");
 
         if (token != null && token.startsWith("Bearer ")) {
-            token = token.substring(7); // Remove "Bearer "
+            token = token.substring(7);
             try {
                 Claims claims = jwtManager.parseToken(token);
-                System.out.println("Token validated successfully: " + claims);
+
+                String email = claims.getSubject();
+                List<String> roles = (List<String>) claims.get(SecurityConstants.JWT_ROLE_KEY);
+
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        email, null, roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
                 filterChain.doFilter(request, response);
             } catch (JwtException e) {
-                System.out.println("Token validation failed: " + e.getMessage());
                 response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
                 response.getWriter().write("Invalid JWT token");
             }
