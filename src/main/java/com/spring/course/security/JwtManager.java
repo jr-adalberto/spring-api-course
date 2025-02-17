4948c0524fa0fb7615379497a1c28d67f4525932
Adalberto da Silva Junior <sbetiin_@hotmail.com>
2025-02-17 20:22:37 -0300

    change method to dto

--- src/main/java/com/spring/course/security/JwtManager.java
+++ src/main/java/com/spring/course/security/JwtManager.java
@@ -1,6 +1,7 @@
 package com.spring.course.security;
 
 import com.spring.course.constant.SecurityConstants;
+import com.spring.course.dto.UserLoginResponsedto;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
@@ -13,25 +14,26 @@ import java.util.List;
 @Component
 public class JwtManager {
 
-    public String createToken(String email, List<String> roles, String secretKey, int days, String jwtProvider) {
+    public UserLoginResponsedto createToken(String email, List<String> roles) {
         Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_MONTH, days);
+        calendar.add(Calendar.DAY_OF_MONTH, SecurityConstants.JWT_EXP_DAYS);
 
-        return Jwts.builder()
+        String jwt = Jwts.builder()
                 .setSubject(email)
                 .setExpiration(calendar.getTime())
                 .claim(SecurityConstants.JWT_ROLE_KEY, roles)
-                .signWith(SignatureAlgorithm.HS512, secretKey.getBytes())
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.API_KEY.getBytes())
                 .compact();
+
+        Long expireIn = calendar.getTimeInMillis();
+
+        return new UserLoginResponsedto(jwt, expireIn, SecurityConstants.JWT_PROVIDER);
     }
 
     public Claims parseToken(String jwt) throws JwtException {
-        Claims claims = Jwts.parser()
+        return Jwts.parser()
                 .setSigningKey(SecurityConstants.API_KEY.getBytes())
                 .parseClaimsJws(jwt)
                 .getBody();
-
-
-        return claims;
     }
 }
